84f546b551ad8ae14d0f06f1c284848e43033f90
Lukasz Szawronski <lukasz.szawronski@gmail.com>
2022-05-16 12:30:01 +0200

    Added some new bootstrap elements

--- src/main/java/pl/lukasz/szawronski/mydriverapi/security/SecurityConfig.java
+++ src/main/java/pl/lukasz/szawronski/mydriverapi/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -46,6 +47,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
 
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/images/**");
     }
 
     @Bean
